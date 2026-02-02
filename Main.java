566debb830854dc2e3e5cfa742ca712ce9a6def0
Jed Martin Arellano <jedmartinarellano0309@gmail.com>
2026-02-02 19:55:26 +0800

    Update Main.java
    main java completed

--- Main.java
+++ Main.java
@@ -5,3 +5,15 @@ public class Main {
         Player player = new Player("Hero", 100, 50, "Fire Slash", weapon);
 
         Enemy enemy = new Enemy("Goblin", 60, 15, 30);
+
+Level level = new Level("Forest", 2, enemy);
+
+        // ONE PRINT RULE (only here)
+        System.out.println(
+            player.identity() + "\n" +
+            weapon.identity() + "\n" +
+            enemy.identity() + "\n" +
+            level.identity()
+        );
+    }
+}
